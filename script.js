0ccaa88336f5993331c79c79a0ff8f39b327b3ee
levius wafula <121118804+leviuswafula@users.noreply.github.com>
2023-10-04 16:04:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function () {
+    // Hide the loading overlay when the website content has fully loaded
+    const loadingOverlay = document.querySelector(".loading-overlay");
+    loadingOverlay.style.display = "none";
+  });
+  
